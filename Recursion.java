3f6b982726d88423958f2baa3919cdad4001ee8b
Jamie Zeng <jzeng3@stuy.edu>
2019-01-31 17:50:54 -0500

    fixed param and description of sqrt function, fixed tests in main

--- Recursion.java
+++ Recursion.java
@@ -1,18 +1,21 @@
 public class Recursion{
   public static void main(String[] args){
-    System.out.println("Square root of 100: " + sqrt(100));
-    System.out.println("Square root of 2: " + sqrt(2));
-    System.out.println("Square root of 0: " + sqrt(0));
-    System.out.println("Square root of 1: " + sqrt(1));
-    System.out.println("Square root of 0.25: " + sqrt(0.25));
+    System.out.println("Square root of 100, tolerance of 0.00001 (0.001%): " + sqrt(100, 0.00001));
+    System.out.println("Square root of 2, tolerance of 0.00001 (0.001%): " + sqrt(2, 0.00001));
+    System.out.println("Square root of 0, tolerance of 0.00001 (0.001%): " + sqrt(0, 0.00001));
+    System.out.println("Square root of 1, tolerance of 0.00001 (0.001%): " + sqrt(1, 0.00001));
+    System.out.println("Square root of 0.25, tolerance of 0.00001 (0.001%): " + sqrt(0.25, 0.00001));
   }
 
   /*
  *@param n any non-negative value you want to take the sqrt of
  *@return the approximate sqrt of n within a tolerance of 0.001%
+ Recursively find the sqrt using Newton's approximation
+     *tolerance is the allowed percent error the squared answer is away from n.
+     *precondition: n is non-negative
  */
-public static double sqrt(double n){
-  return sqrtH(n, 1.0, 0.001);
+public static double sqrt(double n, double tolerance){
+  return sqrtH(n, 1.0, tolerance);
 }
 /*
 *@param n any non-negative value you want to take the sqrt of
@@ -20,7 +23,7 @@ public static double sqrt(double n){
 *@param tolerance the percentage of error between actual sqrt and guess
 *@returns approximate sqrt of n within given tolerance
 */
-public static double sqrtH(double n, double guess, double tolerance){
+private static double sqrtH(double n, double guess, double tolerance){
    if (n == 0){
      return 0;
   }
