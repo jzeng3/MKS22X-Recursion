f656df6e9cf0a4614a23f23417dce961fc6a249b
Jamie Zeng <jzeng3@stuy.edu>
2019-01-31 18:01:36 -0500

    added code structure for fib

--- Recursion.java
+++ Recursion.java
@@ -35,4 +35,21 @@ private static double sqrtH(double n, double guess, double tolerance){
     return guess;
   }
 }
+
+/*Recursively find the n'th fibbonaci number in linear time
+     *fib(0) = 1; fib(1) = 1; fib(5) = 5
+     *precondition: n is non-negative
+     */
+    public static int fib(int n){
+      return 0;
+    }
+
+    private static int fibH(int n, long ans){
+      if (n <= 1){
+        return ans;
+      }
+      else{
+        
+      }
+    }
 }
